f68fd3f2cc687febbcc72e4e970d9a8185fb0639
Fred <68379946+Fredfredster@users.noreply.github.com>
2020-11-19 20:28:27 +0000

    Major overhaul
    Updated file locations. Tweaked the name generator for file path checks. Ready to start on __init__.py.

---
+++ __init__.py
@@ -0,0 +1,2 @@
+
+import name_finder as namefinder
--- name_finder.py
+++ name_finder.py
@@ -10,55 +10,44 @@ __notes__ = 'This is a script used for Life Simulator.'
 
 
 
-def get_name(filename1='firstnames.txt', filename2='lastnames.txt'):
-	lines = 0
-	try:
-		with open('./names/{0}'.format(filename1)) as f:
-			f.close()
+def get_name(filename1=None, filename2=None):
+	if filename1 == None or filename2 == None:
+		return 'You must provide valid paths to the documents you want to scan for names'
+	else:
+		lines = 0
 		try:
-			lines = open('./names/{0}'.format(filename1)).read().splitlines()
-			if lines == []:
-				return '{0} and/or {0} is empty'.format(filename1, filename2)
-			else:
-				result = str(random.choice(lines))
-				lines = 0
-				try:
-					with open('./names/{0}'.format(filename2)) as f:
-						f.close()
+			with open(filename1) as f:
+				f.close()
+			try:
+				lines = open(filename1).read().splitlines()
+				if lines == []:
+					return '{0} is empty'.format(filename1)
+				else:
+					result = str(random.choice(lines))
+					lines = 0
 					try:
-						lines = open('./names/{0}'.format(filename2)).read().splitlines()
-						if lines == []:
-							return '{0} is empty'.format(filename2)
-						else:
-							result = '{0} {1}'.format(result, str(random.choice(lines)))
-							return result
-					except Exception as e:
-						raise Exception(e)
-				except:
-					return '{0} not found.'.format(filename2)
-		except Exception as e:
-			raise Exception(e)
-	except:
-		return '{0} not found.'.format(filename1)
+						with open(filename2) as f:
+							f.close()
+						try:
+							lines = open(filename2).read().splitlines()
+							if lines == []:
+								return '{0} is empty'.format(filename2)
+							else:
+								result = '{0} {1}'.format(result, str(random.choice(lines)))
+								return result
+						except Exception as e:
+							raise Exception(e)
+					except:
+						return '{0} not found.'.format(filename2)
+			except Exception as e:
+				raise Exception(e)
+		except:
+			return '{0} not found.'.format(filename1)
 
 
 def get_firstname(filename=None):
 	if filename == None:
-		filename = 'firstnames.txt'
-		lines = 0
-		try:
-			with open('./names/{0}'.format(filename)) as f:
-				f.close()
-			try:
-				lines = open('./names/{0}'.format(filename)).read().splitlines()
-				if lines == []:
-					return '{0} is empty'.format(filename)
-				else:
-					return random.choice(lines)
-			except Exception as e:
-				raise Exception(e)
-		except:
-			return '{0} not found'.format(filename)
+		return 'You must provide a valid path to the document you want to scan for names'
 	else:
 		lines = 0
 		try:
@@ -78,21 +67,7 @@ def get_firstname(filename=None):
 
 def get_lastname(filename=None):
 	if filename == None:
-		filename = 'lastnames.txt'
-		lines = 0
-		try:
-			with open('./names/{0}'.format(filename)) as f:
-				f.close()
-			try:
-				lines = open('./names/{0}'.format(filename)).read().splitlines()
-				if lines == []:
-					return '{0} is empty'.format(filename)
-				else:
-					return random.choice(lines)
-			except Exception as e:
-				raise Exception(e)
-		except:
-			return '{0} not found'.format(filename)
+		return 'You must provide a valid path to the document you want to scan for names'
 	else:
 		lines = 0
 		try:
@@ -109,6 +84,4 @@ def get_lastname(filename=None):
 		except:
 			return '{0} not found'.format(filename) 
 	
-		
-
-print(get_name('lastnames.txt', 'firstnames.txt'))
+	
